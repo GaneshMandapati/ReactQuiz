072fcaffd45257b0955a9af325e3744fd3567f07
Ganesh <mandapatiganesh389@gmail.com>
2020-07-10 13:00:01 +0530

    Updates

---
+++ precache-manifest.cf877dbf58b4667040e0616aa96e0bc6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c848ed63d341abb0e649b87c537fd8d5",
+    "url": "/GaneshMadapati/ReactQuiz/index.html"
+  },
+  {
+    "revision": "c88bd4ecbe363925f328",
+    "url": "/GaneshMadapati/ReactQuiz/static/css/main.a41996f2.chunk.css"
+  },
+  {
+    "revision": "fca835239e249747f77a",
+    "url": "/GaneshMadapati/ReactQuiz/static/js/2.6330f71d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/GaneshMadapati/ReactQuiz/static/js/2.6330f71d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c88bd4ecbe363925f328",
+    "url": "/GaneshMadapati/ReactQuiz/static/js/main.56a2ad5d.chunk.js"
+  },
+  {
+    "revision": "c394ac047c8964deebcb",
+    "url": "/GaneshMadapati/ReactQuiz/static/js/runtime-main.ef413b7e.js"
+  }
+]);
